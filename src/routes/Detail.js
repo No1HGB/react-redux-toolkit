58103f9befc47be2756a5e8bad7cc217bcfb012f
HGB <hsw8147@gmail.com>
2022-07-12 22:53:12 +0900

    react-redux-JS

---
+++ src/routes/Detail.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { actionCreators } from "../store";
+
+function Detail({ toDos, onBtnClick }) {
+  const id = useParams().id;
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
+  const navigate = useNavigate();
+
+  const handleDel = () => {
+    onBtnClick(id);
+    navigate("/");
+  };
+  return (
+    <h2>
+      <div>{toDo?.text}</div>
+      <div>Created at: {toDo?.id}</div>
+      <button onClick={handleDel}>DEL</button>
+    </h2>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onBtnClick: (id) => dispatch(actionCreators.deleteToDo(id)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
