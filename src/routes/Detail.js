5fe6dc859c1dd032c865d233ec4305730109f454
HGB <hsw8147@gmail.com>
2022-07-13 01:35:15 +0900

    redux/toolkit JS

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import { actionCreators } from "../store";
+import { remove } from "../store";
 
 function Detail({ toDos, onBtnClick }) {
   const id = useParams().id;
@@ -27,7 +27,7 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch, ownProps) {
   return {
-    onBtnClick: (id) => dispatch(actionCreators.deleteToDo(id)),
+    onBtnClick: (id) => dispatch(remove(id)),
   };
 }
 
